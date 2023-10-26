2809991d5e56bab2d3f2ab27a51a6cef9d5754d7
Randy Reddig <randy.reddig@fastly.com>
2023-10-26 16:35:02 -0700

    wit/gen: scaffolding for generating a series of Go packages and files

---
+++ wit/gen/gen_test.go
@@ -0,0 +1,22 @@
+package gen
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestFileString(t *testing.T) {
+	f := &File{
+		Build: "wasm || wasm32 || tinygo.wasm",
+		Package: &Package{
+			Path: "wasm/wasi/clocks/wallclock",
+			Name: "wallclock",
+		},
+		Imports: map[string]string{
+			"encoding/json": "json",
+			"io":            "io",
+		},
+	}
+
+	fmt.Println(f.String())
+}
