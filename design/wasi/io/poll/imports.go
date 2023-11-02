31280dd55429d28333b343a38b8ec17c414508c0
Randy Reddig <randy.reddig@fastly.com>
2023-11-02 14:16:06 -0700

    design/wasi/io/poll: improve comments

--- design/wasi/io/poll/imports.go
+++ design/wasi/io/poll/imports.go
@@ -7,6 +7,9 @@ import (
 	"unsafe"
 )
 
+// Imports (if wasi:io/poll was imported)
+
+// Poll is the Go implemention of WIT func "wasi:io/poll.poll".
 func Poll(in []Pollable) []uint32 {
 	ptr, size := wasmimport_poll(unsafe.SliceData(in), uint32(len(in)))
 	runtime.KeepAlive(in) // TODO: necessary?
@@ -18,6 +21,7 @@ func Poll(in []Pollable) []uint32 {
 //go:wasmimport wasi:io/poll poll
 func wasmimport_poll(data *Pollable, size uint32) (*uint32, int32)
 
+// Block is the Go implemention of WIT method "wasi:io/poll.pollable.block".
 func (self Pollable) Block() {
 	wasmimport_pollable_block()
 }
@@ -25,6 +29,7 @@ func (self Pollable) Block() {
 //go:wasmimport wasi:io/poll [method]pollable.block
 func wasmimport_pollable_block()
 
+// Ready is the Go implemention of WIT method "wasi:io/poll.pollable.ready".
 func (self Pollable) Ready() bool {
 	return wasmimport_pollable_ready()
 }
