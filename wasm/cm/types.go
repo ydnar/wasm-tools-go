82317095675e652695086bb1c1481ec13623b933
Randy Reddig <randy.reddig@fastly.com>
2023-11-02 12:02:43 -0700

    wasm/cm: importable Component Model primitive types

---
+++ wasm/cm/types.go
@@ -0,0 +1,22 @@
+package cm
+
+type Bool bool
+
+type S8 int8
+type U8 uint8
+type S16 int16
+type U16 uint16
+type S32 int32
+type U32 uint32
+type S64 int64
+type U64 uint64
+type F32 float32
+type F64 float64
+
+type Char rune
+type String string
+
+type Resource U32
+
+type Borrow[T ~uint32] U32
+type Own[T ~uint32] U32
