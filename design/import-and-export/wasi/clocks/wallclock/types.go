49ce1b9ba4069e4a03dd3a295f55b7bca8f1136c
Randy Reddig <randy.reddig@fastly.com>
2023-11-02 14:14:14 -0700

    design/import-and-export: remove build tags where not necessary

--- design/import-and-export/wasi/clocks/wallclock/types.go
+++ design/import-and-export/wasi/clocks/wallclock/types.go
@@ -1,16 +1,12 @@
-//go:build wasm
-
 package wallclock
 
-// Interface is the exported Go interface of
-// WIT interface "wasi:clocks/wall-clock".
+// Interface is the Go implementation of WIT interface "wasi:clocks/wall-clock".
 type Interface interface {
 	Now() DateTime
 	Resolution() DateTime
 }
 
-// DateTime is a Go implementation of the
-// WIT type "wasi:clocks/wall-clock.datetime".
+// DateTime is a Go implementation of WIT type "wasi:clocks/wall-clock.datetime".
 type DateTime struct {
 	Seconds      uint64
 	Nanonseconds uint32
