63e47339ffeebccf2f0a6bb9c0388bf22726f88e
Randy Reddig <randy.reddig@fastly.com>
2023-11-03 10:25:06 -0700

    design/export/wasi/io/poll: exploring own/borrow semantics

--- design/export/wasi/io/poll/exports.go
+++ design/export/wasi/io/poll/exports.go
@@ -1,22 +1,29 @@
-//go:build wasm
-
 package poll
 
 import (
 	"unsafe"
+
+	"github.com/ydnar/wasm-tools-go/wasm/cm"
 )
 
 // Interface implements WIT interface "wasi:io/poll".
 type Interface interface {
-	Poll(in []Pollable) []uint32
-	Pollable() interface {
-		Block(self Pollable)
-		Ready(self Pollable) bool
-	}
+	Poll(in []cm.Borrow[Pollable]) []uint32
+	Pollable() PollableInterface
+}
+
+type PollableInterface interface {
+	Own(cm.Own[Pollable]) PollableMethods
+	Borrow(cm.Borrow[Pollable]) PollableMethods
+}
+
+type PollableMethods interface {
+	Block()
+	Ready() bool
 }
 
 // Pollable implements WIT type "wasi:io/poll.pollable".
-type Pollable uint32
+type Pollable cm.Resource
 
 // Export registers a concrete implementation of WIT interface "wasi:io/poll".
 func Export(i Interface) {
@@ -27,17 +34,17 @@ func Export(i Interface) {
 var impl Interface
 
 //go:wasmexport wasi:io/poll poll
-func wasmexport_poll(data *Pollable, size uint32) (*uint32, uint32) {
+func wasmexport_poll(data *cm.Borrow[Pollable], size uint32) (*uint32, uint32) {
 	s := impl.Poll(unsafe.Slice(data, size))
 	return unsafe.SliceData(s), uint32(len(s)) // FIXME: will this work with Go GC?
 }
 
 //go:wasmexport wasi:io/poll [method]pollable.block
-func wasmexport_method_pollable_block(self Pollable) {
-	impl.Pollable().Block(self)
+func wasmexport_method_pollable_block(self cm.Borrow[Pollable]) {
+	impl.Pollable().Borrow(self).Block()
 }
 
 //go:wasmexport wasi:io/poll [method]pollable.ready
-func wasmexport_method_pollable_ready(self Pollable) bool {
-	return impl.Pollable().Ready(self)
+func wasmexport_method_pollable_ready(self cm.Borrow[Pollable]) bool {
+	return impl.Pollable().Borrow(self).Ready()
 }
